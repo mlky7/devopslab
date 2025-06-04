6f0589f53f2ed8f2b955e6b31a595db0abf00ffb
mlky7 <120398635+mlky7@users.noreply.github.com>
2025-06-04 10:47:30 +0530

    Update App.java

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -1,11 +1,17 @@
-package org.example.app;
+package com.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        System.out.println("Hello and welcome!");
+        int x = 5;
+        int y = 3;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("The sum of " + x +  " and " + y + " is " + add(x,y));
+    }
+    public static int add(int x, int y){
+        return x + y;
     }
 }
